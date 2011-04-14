df51002fc2ab235b3b42df1a84165b4257f3219d
wlk-ubuntu <wonglapk@gmail.com>
2011-04-14 19:23:55 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogMongomapper::Application.config.secret_token = 'b071957ceaf1d439502547aed4f5ab0d3ae438b23a221223a04b4af78e9a66c49815c6b1eba9125ff8a06087ef2f7350b83e286e25608001e0d6db8689fdda4d'
